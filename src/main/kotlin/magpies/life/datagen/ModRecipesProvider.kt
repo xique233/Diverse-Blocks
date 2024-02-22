ae8563f7afe272da48bb2f2bdd5b9a508b5eed64
Magpie <320718257@qq.com>
2024-02-23 00:04:52 +0800

    注册合成表小修改

--- src/main/kotlin/magpies/life/datagen/ModRecipesProvider.kt
+++ src/main/kotlin/magpies/life/datagen/ModRecipesProvider.kt
@@ -1,9 +1,12 @@
 package magpies.life.datagen
 
 import magpies.life.DiverseBlocks
+import magpies.life.registry.ModBlock
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
+import net.minecraft.block.Blocks
 import net.minecraft.data.server.recipe.RecipeExporter
+import net.minecraft.data.server.recipe.RecipeProvider
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
 import net.minecraft.item.Items
 import net.minecraft.recipe.book.RecipeCategory
@@ -11,9 +14,18 @@ import net.minecraft.util.Identifier
 
 class ModRecipesProvider(output: FabricDataOutput) : FabricRecipeProvider(output) {
     override fun generate(exporter: RecipeExporter) {
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, Items.SUGAR, 6)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlock.DIRT_SLAB, 6)
             .pattern("###")
             .input('#', Items.DIRT)
+            .criterion(hasItem(Blocks.DIRT), RecipeProvider.conditionsFromItem(Blocks.DIRT))
+            .offerTo(exporter, Identifier(DiverseBlocks.MOD_ID,"dirt_slab"))
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlock.DIRT_STAIRS, 4)
+            .pattern("#  ")
+            .pattern("## ")
+            .pattern("###")
+            .input('#', Items.DIRT)
+            .criterion(hasItem(Blocks.DIRT), RecipeProvider.conditionsFromItem(Blocks.DIRT))
             .offerTo(exporter, Identifier(DiverseBlocks.MOD_ID,"dirt_stairs"))
     }
 }
